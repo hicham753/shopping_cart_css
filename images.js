daa6153b38cbbc4b7fdcae72e4c45340efbea087
hicham <hichamlakhdar44@gmail.com>
2024-03-21 13:47:08 +0000

    create shopping cart html & css

---
+++ images.js
@@ -0,0 +1,5 @@
+const images = [
+    "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img5.webp",
+    "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img6.webp",
+    "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img7.webp"
+]
